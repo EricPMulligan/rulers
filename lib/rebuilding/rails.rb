6d939227b52e25d276fd4b3d4f85bd7b4e1a57e5
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-11-24 08:01:02 -0500

    Rewrote from Rebuilding::Rails to Rulers.

--- lib/rebuilding/rails.rb
+++
@@ -1,7 +0,0 @@
-require "rebuilding/rails/version"
-
-module Rebuilding
-  module Rails
-    # Your code goes here...
-  end
-end
--- lib/rebuilding/rails/version.rb
+++
@@ -1,5 +0,0 @@
-module Rebuilding
-  module Rails
-    VERSION = "0.1.0"
-  end
-end
