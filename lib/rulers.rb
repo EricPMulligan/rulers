943007cbf632bb24dccceb16514946b441e1d92e
Eric Mulligan <eric.pierre.mulligan@gmail.com>
2016-11-26 16:57:18 -0500

    Finished chapter 6.

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -9,8 +9,7 @@ module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404,
-                { 'Content-Type' => 'text/html' }, []]
+        return [404, { 'Content-Type' => 'text/html' }, []]
       elsif env['PATH_INFO'] == '/'
         controller = HomeController.new(env)
         text = controller.send('index')
@@ -21,7 +20,12 @@ module Rulers
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-        [200, { 'Content-Type' => 'text/html' }, [text]]
+        r = controller.get_response(act)
+        if r
+          [r.status, r.headers, [r.body].flatten]
+        else
+          [200, { 'Content-Type' => 'text/html' }, [text]]
+        end
       rescue StandardError => e
         text = '<p>An error occurred.</p>'
         text += "<p>#{e.message}</p>"
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,5 +1,6 @@
 require 'erubis'
 require 'rulers/file_model'
+require 'rack/request'
 
 module Rulers
   class Controller
@@ -9,26 +10,44 @@ module Rulers
       @env = env
     end
 
-    def env
-      @env
-    end
-
-    def render(view_name, locals = {})
-      filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
-      template = File.read filename
-      eruby = Erubis::Eruby.new(template)
-      self.instance_variables.each { |variable| locals[variable] = self.instance_variable_get variable }
-      eruby.result locals
-    end
-
     def controller_name
       klass = self.class
       klass = klass.to_s.gsub /Controller$/, ''
       Rulers.to_underscore klass
     end
 
+    def env
+      @env
+    end
+
+    def get_response(action)
+      render(action) unless @response
+      @response
+    end
+
+    def params
+      request.params
+    end
+
     def request
       @_request ||= Rack::Request.new @env
+    end
+
+    def render(*args)
+      action, locals = args
+      locals = {} unless locals
+      filename = File.join 'app', 'views', controller_name, "#{action}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      self.instance_variables.each { |variable| locals[variable] = self.instance_variable_get variable }
+      text = eruby.result locals
+      response(text)
+    end
+
+    def response(text, status = 200, headers = {})
+      raise 'Already responded!' if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
     end
   end
 end
